2ff5859252136fb3a91673bf887d4bd494ee775c
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-01-18 20:35:07 -0500

    fixed server issue in package.json

--- app/src/components/TableGrid.jsx
+++ app/src/components/TableGrid.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Grid } from '@mantine/core';
 import CommentTable from './tables/comment/CommentTable';
 import { Button } from '@mantine/core';
@@ -6,16 +5,16 @@ import { Button } from '@mantine/core';
 const TableGrid = () => {
     return (
         <>
-        {/* <div style={{display: 'flex', justifyContent: "center"}}> */}
-        <Grid>
-            <Grid.Col span={6}><CommentTable /></Grid.Col>
-            <Grid.Col span={6}><CommentTable /></Grid.Col>
-            <Grid.Col span={6}><CommentTable /></Grid.Col>
-            <Grid.Col span={6}><CommentTable /></Grid.Col>
-        </Grid>
-        {/* </div> */}
-        
-        <Button>Button</Button>
+            {/* <div style={{display: 'flex', justifyContent: "center"}}> */}
+            <Grid>
+                <Grid.Col span={6}><CommentTable /></Grid.Col>
+                <Grid.Col span={6}><CommentTable /></Grid.Col>
+                <Grid.Col span={6}><CommentTable /></Grid.Col>
+                <Grid.Col span={6}><CommentTable /></Grid.Col>
+            </Grid>
+            {/* </div> */}
+
+            <Button>Button</Button>
         </>
     )
 }
