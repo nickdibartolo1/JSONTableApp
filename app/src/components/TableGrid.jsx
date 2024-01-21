77abf57ede4ce1b82c6b9dec562bba1f82c7fdd7
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-01-21 16:54:23 -0500

    added 1st data table to grid

--- app/src/components/TableGrid.jsx
+++ app/src/components/TableGrid.jsx
@@ -4,11 +4,13 @@ import CommentTable from './tables/comment/CommentTable';
 
 const TableGrid = () => {
     return (
-        <Grid>
-            <Grid.Col span={4}><CommentTable/></Grid.Col>
-            <Grid.Col span={4}>2</Grid.Col>
-            <Grid.Col span={4}>3</Grid.Col>
-        </Grid>
+        <main>
+            <Grid gutter={{ base: 5, xs: 'md', md: 'xl', xl: 50 }}>
+                <Grid.Col style={{ backgroundColor: "gray" }} span={4}><CommentTable /></Grid.Col>
+                <Grid.Col span={4}>2</Grid.Col>
+                <Grid.Col span={4}>3</Grid.Col>
+            </Grid>
+        </main>
     )
 }
 
--- app/src/components/tables/comment/CommentTable.jsx
+++ app/src/components/tables/comment/CommentTable.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
-import { Table, ScrollArea } from '@mantine/core';
-import cx from 'clsx';
+import { Table } from '@mantine/core';
+
 import classes from "./TableScrollArea.module.css"
 
 const CommentTable = () => {
   const [data, setData] = useState([]);
-  const [scrolled, setScrolled] = useState(false);
+
 
   useEffect(() => {
     async function getResponse() {
@@ -26,25 +26,33 @@ const CommentTable = () => {
     getResponse();
 
   }, [])
-  
-  const rows = data.map((row) => (<Table.Tr key={row.name}>
-    <Table.Td>{row.name}</Table.Td>
-    <Table.Td>{row.email}</Table.Td>
-    <Table.Td>{row.company}</Table.Td>
-  </Table.Tr>));
+
+  const rows = data.map((row) => {
+    return (
+      <Table.Tr key={row.title}>
+        <Table.Td>
+          {row.name}
+        </Table.Td>
+        <Table.Td>
+          {row.email}
+        </Table.Td>
+      </Table.Tr>
+    );
+  });
+
   return (
-    <ScrollArea h={300} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
-      <Table miw={700}>
-        <Table.Thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
+    <div className={classes.layout}>
+      <Table verticalSpacing="sm">
+        <Table.Thead>
           <Table.Tr>
-            <Table.Th>Name</Table.Th>
-            <Table.Th>Email</Table.Th>
             <Table.Th>Company</Table.Th>
+            <Table.Th>Email</Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>{rows}</Table.Tbody>
       </Table>
-    </ScrollArea>);
+    </div>
+  );
 }
 
 export default CommentTable;
